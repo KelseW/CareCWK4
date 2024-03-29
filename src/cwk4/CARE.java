19f30064ca7145a7a18bdc4b377e84606f92411b
Kelsey <kwehmi97@hotmail.com>
2024-03-29 12:38:53 +0000

    test1

--- src/cwk4/CARE.java
+++ src/cwk4/CARE.java
@@ -22,7 +22,8 @@ public interface CARE extends Serializable
      * including the name of the vizier, state of the treasury,
      * whether defeated or not, and the champions currently in the 
      * team,(or, "No champions" if team is empty)
-     * ur a bitch
+     *
+     * hello test
      **/
     public String toString();
     
